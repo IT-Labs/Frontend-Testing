b131fff0957df33fb47140afd8808f0743255ffb
Voislav Mishevski <voislav.mishevski@it-labs.com>
2020-06-11 00:58:39 +0200

    add spectator

--- ng-unit-testing/src/app/todo-app/components/todo-item/todo-item.component.spec.ts
+++ ng-unit-testing/src/app/todo-app/components/todo-item/todo-item.component.spec.ts
@@ -1,28 +1,57 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
 import { TodoItemComponent } from './todo-item.component';
+import { Todo } from '../../models/Todo';
 
 describe('TodoItemComponent', () => {
-  let component: TodoItemComponent;
-  let fixture: ComponentFixture<TodoItemComponent>;
+  const getTodo = () => ({
+    id: '1',
+    title: 'Wash the dishes',
+    isCompleted: false,
+    createdAt: '2020-06-02T12:20:56.512Z',
+  } as Todo);
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TodoItemComponent ]
-    })
-    .compileComponents();
-  }));
+  describe('native angular TestBed', () => {
+    let component: TodoItemComponent;
+    let fixture: ComponentFixture<TodoItemComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TodoItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ TodoItemComponent ]
+      })
+      .compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(TodoItemComponent);
+      component = fixture.componentInstance;
+      component.todo = getTodo();
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  describe('spectator', () => {
+    const getInputs = (props: Partial<TodoItemComponent> = {}) => {
+      return {
+        todo: getTodo(),
+      } as Partial<TodoItemComponent>;
+    }
+    const createComponent = createComponentFactory(TodoItemComponent);
+    const getComponent = (props: Partial<TodoItemComponent> = {}) => {
+      return createComponent({
+        props,
+       });
+    }
+
+    it('should rencer success', () => {
+      const inputs = getInputs();
+      const spectator = getComponent(inputs);
+      expect(spectator).toBeTruthy();
+    });
+  })
 });
